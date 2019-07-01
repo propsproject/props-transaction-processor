918c6b8a9a60a37116e6e2a2627d5d818a1ca582
jon <jon@younow.com>
2019-07-01 04:34:23 -0400

    added signature checking for valid apps and validators and signature to appId validations + updated unit tests

--- core/rpc/balance_update.go
+++ core/rpc/balance_update.go
@@ -7,7 +7,7 @@ import (
 	"github.com/propsproject/sawtooth-go-sdk/protobuf/processor_pb2"
 )
 
-var balanceUpdateHandle = func(request *processor_pb2.TpProcessRequest, context *processor.Context, rpcReq *pending_props_pb.RPCRequest) error {
+var balanceUpdateHandle = func(request *processor_pb2.TpProcessRequest, context *processor.Context, rpcReq *pending_props_pb.RPCRequest, address string) error {
 	balanceUpdate, err := decodeBalanceUpdateRequest(rpcReq)
 	if err != nil {
 		return &processor.InvalidTransactionError{Msg: err.Error()}
--- core/rpc/wallet_link.go
+++ core/rpc/wallet_link.go
@@ -1,18 +1,22 @@
 package rpc
 
 import (
+	"fmt"
 	"github.com/propsproject/sawtooth-go-sdk/protobuf/processor_pb2"
 	"github.com/propsproject/sawtooth-go-sdk/processor"
 	"github.com/propsproject/props-transaction-processor/core/proto/pending_props_pb"
 	"github.com/propsproject/props-transaction-processor/core/state"
 )
 
-var walletLinkHandle = func(request *processor_pb2.TpProcessRequest, context *processor.Context, rpcReq *pending_props_pb.RPCRequest) error {
+var walletLinkHandle = func(request *processor_pb2.TpProcessRequest, context *processor.Context, rpcReq *pending_props_pb.RPCRequest, address string) error {
 	walletLinks, err := decodeWalletLinkRequest(rpcReq)
 	if err != nil {
 		return &processor.InvalidTransactionError{Msg: err.Error()}
 	}
-
+	if address != walletLinks.GetUsers()[0].GetApplicationId() {
+		logger.Infof("Signer address %v does not match applicationId %v", address, walletLinks.GetUsers()[0].GetApplicationId())
+		return &processor.InvalidTransactionError{Msg: fmt.Sprintf("Signer address %v does not match applicationId %v", address, walletLinks.GetUsers()[0].GetApplicationId())}
+	}
 	return state.NewState(context).SaveWalletLink(walletLinks)
 }
 
