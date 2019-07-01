bbedc7c1323dfe09ad02405f1c8aa51c1d4eaa89
Donald DeWulf <dewulf@younow.com>
2019-07-01 14:43:55 +0300

    Fix for logs

--- core/state/state.go
+++ core/state/state.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hyperledger/sawtooth-sdk-go/processor"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"sync"
 )
 
 type State struct {
@@ -13,13 +14,20 @@ type State struct {
 var myLogger, _ = zap.NewProduction()
 var logger = myLogger.Sugar()
 
-func NewState(context *processor.Context) *State {
+var doOnce sync.Once
 
-	logger = logger.With(
-		zap.String("app", viper.GetString("app")),
-		zap.String("name", viper.GetString("name")),
-		zap.String("env", viper.GetString("environment")),
-	)
+func NewState(context *processor.Context) *State {
+	initLogger()
 
 	return &State{context: context}
+}
+
+func initLogger() {
+	doOnce.Do(func() {
+		logger = logger.With(
+			zap.String("app", viper.GetString("app")),
+			zap.String("name", viper.GetString("name")),
+			zap.String("env", viper.GetString("environment")),
+		)
+	})
 }
