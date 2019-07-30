76a7dfb1e2e0deb1b9b95f4b72cc1bb2d91b1a10
Donald DeWulf <dewulf@younow.com>
2019-07-30 12:40:30 +0300

    Add logs on utils

--- core/eth-utils/transfer_details_log.go
+++ core/eth-utils/transfer_details_log.go
@@ -6,9 +6,13 @@ import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/hyperledger/sawtooth-sdk-go/logging"
 	"github.com/propsproject/goprops-toolkit/propstoken/bindings/token"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"math/big"
+	"os"
+	"sync"
 )
 
 type TransferDetails struct {
@@ -38,8 +42,17 @@ type SettlementEvent struct {
 	RewardsAddress    common.Address
 }
 
+var atom = zap.NewAtomicLevel()
+var encoderCfg = zap.NewProductionEncoderConfig()
+var logger *zap.SugaredLogger
+
+var doOnce sync.Once
+
 func GetEthTransactionTransferDetails(transactionHash string, address string, client *propstoken.Client) (*TransferDetails, uint64, error) {
-	logging.Get().Infof("GetEthTransactionTransferDetails TransactionHash %s ((balanceUpdate for %v)", transactionHash, address)
+
+	initLogger()
+	
+	logger.Infof("GetEthTransactionTransferDetails TransactionHash %s ((balanceUpdate for %v)", transactionHash, address)
 	transaction, err := client.RPC.TransactionReceipt(context.Background(), common.HexToHash(transactionHash))
 	if err != nil {
 		return nil, 0, fmt.Errorf("unable to get transaction receipt for hash (%s)", err)
@@ -51,15 +64,15 @@ func GetEthTransactionTransferDetails(transactionHash string, address string, cl
 		if sig == "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef" { // only if matches Transfer signature keccak256(Transfer(address,address,uint256))
 			from := NormalizeAddress(fmt.Sprintf("0x%v", topics[1].Hex()[26:]))
 			to := NormalizeAddress(fmt.Sprintf("0x%v", topics[2].Hex()[26:]))
-			logging.Get().Infof("GetEthTransactionTransferDetails checking addresses from=0x%v, to=0x%v address=%v", from, to, address)
+			logger.Infof("GetEthTransactionTransferDetails checking addresses from=0x%v, to=0x%v address=%v", from, to, address)
 			if address == from || address == to {
 
 			} else {
 				if (from == "0x0000000000000000000000000000000000000000" && to != address) || (to == "0x0000000000000000000000000000000000000000" && from != address) {
-					logging.Get().Infof("GetEthTransactionTransferDetails skipping due to from/to address being 0 (balanceUpdate for %v)", address)
+					logger.Infof("GetEthTransactionTransferDetails skipping due to from/to address being 0 (balanceUpdate for %v)", address)
 					continue
 				}
-				logging.Get().Infof("transaction %v address does not match reported balance update address %v, %v do not match %v", transactionHash, from, to, address)
+				logger.Infof("transaction %v address does not match reported balance update address %v, %v do not match %v", transactionHash, from, to, address)
 				return nil, 0, fmt.Errorf("transaction %v address does not match reported balance update address %v, %v do not match %v", transactionHash, from, to, address)
 			}
 
@@ -69,10 +82,10 @@ func GetEthTransactionTransferDetails(transactionHash string, address string, cl
 			}
 			balance, err := client.Token.BalanceOf(&callOptions, common.HexToAddress(address))
 			if err != nil {
-				logging.Get().Infof("unable to get balanceOf %v on block %v (%s)", address, log.BlockNumber, err)
+				logger.Infof("unable to get balanceOf %v on block %v (%s)", address, log.BlockNumber, err)
 				return nil, 0, fmt.Errorf("unable to get balanceOf %v on block %v (%s)", address, log.BlockNumber, err)
 			} else {
-				logging.Get().Infof("GetEthTransactionTransferDetails balance %v blockNumber %v (log.BlockNumber %v)", balance.String(), callOptions.BlockNumber.String(), log.BlockNumber)
+				logger.Infof("GetEthTransactionTransferDetails balance %v blockNumber %v (log.BlockNumber %v)", balance.String(), callOptions.BlockNumber.String(), log.BlockNumber)
 			}
 
 			transferDetails := TransferDetails{
@@ -82,19 +95,22 @@ func GetEthTransactionTransferDetails(transactionHash string, address string, cl
 				From: common.HexToAddress(from),
 				To: common.HexToAddress(to),
 			}
-			logging.Get().Infof("GetEthTransactionTransferDetails got transfer details amount=%v (balanceUpdate for %v)", transferDetails.Amount.String(), address)
+			logger.Infof("GetEthTransactionTransferDetails got transfer details amount=%v (balanceUpdate for %v)", transferDetails.Amount.String(), address)
 			return &transferDetails, log.BlockNumber, nil
 		} else {
-			logging.Get().Infof("GetEthTransactionTransferDetails signature didn't match transfer got sig=%v (balanceUpdate for %v)", sig, address)
+			logger.Infof("GetEthTransactionTransferDetails signature didn't match transfer got sig=%v (balanceUpdate for %v)", sig, address)
 		}
 	}
-	logging.Get().Infof("unable to get TransferDetails data from transaction (%s) (balanceUpdate for %v)", transactionHash, address)
+	logger.Infof("unable to get TransferDetails data from transaction (%s) (balanceUpdate for %v)", transactionHash, address)
 	return nil, 0, fmt.Errorf("unable to get TransferDetails data from transaction (%s)", transactionHash)
 }
 
 
 func GetEthTransactionSettlementDetails(transactionHash string, client *propstoken.Client) (*SettlementDetails, uint64, error) {
-	logging.Get().Infof("GetEthTransactionSettlementDetails TransactionHash %s", transactionHash)
+
+	initLogger()
+	
+	logger.Infof("GetEthTransactionSettlementDetails TransactionHash %s", transactionHash)
 	transaction, err := client.RPC.TransactionReceipt(context.Background(), common.HexToHash(transactionHash))
 	if err != nil {
 		return nil, 0, fmt.Errorf("unable to get transaction receipt for hash (%s)", err)
@@ -103,12 +119,12 @@ func GetEthTransactionSettlementDetails(transactionHash string, client *propstok
 	for _, log := range transaction.Logs {
 		topics := log.Topics
 		sig := topics[0].Hex()
-		logging.Get().Infof("GetEthTransactionSettlementDetails sig=%v", sig)
+		logger.Infof("GetEthTransactionSettlementDetails sig=%v", sig)
 		if sig == "0x53b5073ff19aef23b167e83c6be14817da210375bec35b4c0ccfc0cded9a23f8" { // only if matches Transfer signature keccak256(Settlement(address,bytes32,address,uint256,address))
 			var settlementEvent SettlementEvent
 			applicationId := common.HexToAddress(topics[1].Hex())
 			userId, err := hex.DecodeString(topics[2].Hex()[2:])
-			logging.Get().Infof("GetEthTransactionSettlementDetails applicationId:%v, userId:%v, err:%v topcis[2].hex:%v", applicationId.String(), string(userId), err, topics[2].Hex()[2:])
+			logger.Infof("GetEthTransactionSettlementDetails applicationId:%v, userId:%v, err:%v topcis[2].hex:%v", applicationId.String(), string(userId), err, topics[2].Hex()[2:])
 			if err != nil {
 				return nil, 0, fmt.Errorf("unable to decode userId %v (%s)", topics[2].Hex(), err)
 			}
@@ -124,10 +140,10 @@ func GetEthTransactionSettlementDetails(transactionHash string, client *propstok
 			}
 			balance, err := client.Token.BalanceOf(&callOptions, to)
 			if err != nil {
-				logging.Get().Infof("GetEthTransactionSettlementDetails> unable to get balanceOf %v on block %v (%s)", to, log.BlockNumber, err)
+				logger.Infof("GetEthTransactionSettlementDetails> unable to get balanceOf %v on block %v (%s)", to, log.BlockNumber, err)
 				return nil, 0, fmt.Errorf("GetEthTransactionSettlementDetails: unable to get balanceOf %v on block %v (%s)", to, log.BlockNumber, err)
 			} else {
-				logging.Get().Infof("GetEthTransactionSettlementDetails: balance %v blockNumber %v (log.BlockNumber %v)", balance.String(), callOptions.BlockNumber.String(), log.BlockNumber)
+				logger.Infof("GetEthTransactionSettlementDetails: balance %v blockNumber %v (log.BlockNumber %v)", balance.String(), callOptions.BlockNumber.String(), log.BlockNumber)
 			}
 
 			settlementDetails := SettlementDetails{
@@ -140,12 +156,36 @@ func GetEthTransactionSettlementDetails(transactionHash string, client *propstok
 
 			}
 
-			logging.Get().Infof("GetEthTransactionSettlementDetails got %v ", settlementDetails)
+			logger.Infof("GetEthTransactionSettlementDetails got %v ", settlementDetails)
 			return &settlementDetails, log.BlockNumber, nil
 		} else {
-			logging.Get().Infof("GetEthTransactionSettlementDetails signature didn't match settlement got sig=%v", sig)
+			logger.Infof("GetEthTransactionSettlementDetails signature didn't match settlement got sig=%v", sig)
 		}
 	}
-	logging.Get().Infof("unable to get SettlementDetails data from transaction (%s)", transactionHash)
+	logger.Infof("unable to get SettlementDetails data from transaction (%s)", transactionHash)
 	return nil, 0, fmt.Errorf("unable to get TransferDetails data from transaction (%s)", transactionHash)
+}
+
+func initLogger() {
+	doOnce.Do(func() {
+		encoderCfg.TimeKey = "timestamp"
+		encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+		atom.SetLevel(zap.DebugLevel)
+
+		var tmpLogger = zap.New(zapcore.NewCore(
+			zapcore.NewJSONEncoder(encoderCfg),
+			zapcore.Lock(os.Stdout),
+			atom,
+		))
+
+		tmpLogger = tmpLogger.With(
+			zap.String("app", viper.GetString("app")),
+			zap.String("name", viper.GetString("name")),
+			zap.String("env", viper.GetString("environment")),
+		)
+
+		defer tmpLogger.Sync()
+
+		logger = tmpLogger.Sugar()
+	})
 }
