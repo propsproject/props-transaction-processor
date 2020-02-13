26aa41fd1741b904652dab30edfb3841445419ec
jon <jon@younow.com>
2020-02-13 09:04:21 -0500

    using propsproject sawtooth sdk

--- core/rpc/settlement.go
+++ core/rpc/settlement.go
@@ -3,8 +3,8 @@ package rpc
 import (
 	"github.com/propsproject/props-transaction-processor/core/proto/pending_props_pb"
 	"github.com/propsproject/props-transaction-processor/core/state"
-	"github.com/hyperledger/sawtooth-sdk-go/processor"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/processor_pb2"
+	"github.com/propsproject/sawtooth-go-sdk/processor"
+	"github.com/propsproject/sawtooth-go-sdk/protobuf/processor_pb2"
 )
 
 var settlementHandle = func(request *processor_pb2.TpProcessRequest, context *processor.Context, rpcReq *pending_props_pb.RPCRequest, address string) error {
--- core/state/wallet.go
+++ core/state/wallet.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/propsproject/props-transaction-processor/core/eth-utils"
 	"github.com/propsproject/props-transaction-processor/core/proto/pending_props_pb"
-	"github.com/hyperledger/sawtooth-sdk-go/processor"
+	"github.com/propsproject/sawtooth-go-sdk/processor"
 	"math/big"
 )
 
